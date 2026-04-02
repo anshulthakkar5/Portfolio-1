214335885a0b0120b6bad8113b4386d222c1ae9a
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-04-01 20:23:24 -0500

    Update AppTest.java

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,35 +7,31 @@ class AppTest {
 
     @Test
     void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        App app = new App();
+        assertNotNull(app.getGreeting());
     }
 
-    
     @Test
-    void appGreetingIsCorrect() {
+    void greetingIsCorrect() {
         App app = new App();
         assertEquals("Welcome to Tic-Tac-Toe!", app.getGreeting());
     }
 
-
     @Test
-    void testBoardPlacement() {
+    void testPlaceMove() {
         Board board = new Board();
         assertTrue(board.place(1, 'X'));
-        assertFalse(board.place(1, 'O')); // already taken
+        assertFalse(board.place(1, 'O'));
     }
 
-
     @Test
-    void testWinner() {
+    void testWinnerRow() {
         Board board = new Board();
         board.place(1, 'X');
         board.place(2, 'X');
         board.place(3, 'X');
         assertTrue(board.isWinner('X'));
     }
-
 
     @Test
     void testDraw() {
@@ -45,5 +41,12 @@ class AppTest {
             board.place(i + 1, moves[i]);
         }
         assertTrue(board.isDraw());
+    }
+
+    @Test
+    void testInvalidMove() {
+        Board board = new Board();
+        assertFalse(board.place(0, 'X'));
+        assertFalse(board.place(10, 'X'));
     }
 }
