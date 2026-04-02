0f6c674d8b0a16bdbd85e4fdb218c6eefb9a19fb
anshulthakkar5 <204286406+anshulthakkar5@users.noreply.github.com>
2026-04-01 19:44:18 -0500

    Update AppTest.java

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,9 +4,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  @Test
-  void appHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-  }
+
+    @Test
+    void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    
+    @Test
+    void appGreetingIsCorrect() {
+        App app = new App();
+        assertEquals("Welcome to Tic-Tac-Toe!", app.getGreeting());
+    }
+
+
+    @Test
+    void testBoardPlacement() {
+        Board board = new Board();
+        assertTrue(board.place(1, 'X'));
+        assertFalse(board.place(1, 'O')); // already taken
+    }
+
+
+    @Test
+    void testWinner() {
+        Board board = new Board();
+        board.place(1, 'X');
+        board.place(2, 'X');
+        board.place(3, 'X');
+        assertTrue(board.isWinner('X'));
+    }
+
+
+    @Test
+    void testDraw() {
+        Board board = new Board();
+        char[] moves = {'X','O','X','X','O','O','O','X','X'};
+        for (int i = 0; i < 9; i++) {
+            board.place(i + 1, moves[i]);
+        }
+        assertTrue(board.isDraw());
+    }
 }
